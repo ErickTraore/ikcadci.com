ac50afffc86de8207de778d6414eec7650dbf74b
root <root@localhost.localdomain>
2022-07-02 05:16:54 +0000

    mise en service

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,7 +8,9 @@
                   </div>
               </div>
       </div>
-    <nav class="navbar navbar-expand-sm navbar-dark">
+      
+      
+      <nav class="navbar navbar-expand-sm navbar-dark">
       <div class="container-fluid">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
           <span class="navbar-toggler-icon"></span>
@@ -45,6 +47,52 @@
         </div>
       </div>
     </nav>
+
+          <nav class="navbar navbar-expand-sm navbar-dark">
+      <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="mynavbar">
+          <ul class="navbar-nav me-auto">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/home">Messagerie</router-link> 
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/page-profil">Profil</router-link>  
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/home-formation">Formations</router-link>  
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/stepper">Inscription</router-link>  
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/maClass">Classe</router-link>  
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" v-if=logged to="/">  <span class="fa fa-user "></span> Connexion</router-link>  
+              <router-link class="nav-link" v-else  to="/logout">Déconnexion</router-link> 
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" v-if= testBool  to="/pageAdmin">Page administrateur</router-link> 
+            </li>
+          </ul>
+          <form class="d-flex">
+            <input class="form-control me-2" type="text" placeholder="Search">
+            <button class="btn btn-primary" type="button">Search</button>
+          </form>
+        </div>
+      </div>
+    </nav>
+ 
+    
+    
+    
+    
+    
+    
+    
       <router-view/>
     </div>
 </template>
--- frontend/src/views/Page-formation-gle.vue
+++ frontend/src/views/Page-formation-gle.vue
@@ -1,31 +1,13 @@
 <template>
-    <div class="app ">
-            <div> 
-                  <img  alt="Logo-groupomania" src="../assets/pub/doc-1.jpg" class="projPrev__groupo__masq"> 
-            </div>
-            <div> 
-                  <img  alt="Logo-groupomania" src="../assets/pub/doc-2.jpg" class="projPrev__groupo__masq"> 
-            </div>
-            <div> 
-                  <img  alt="Logo-groupomania" src="../assets/pub/doc-3.jpg" class="projPrev__groupo__masq"> 
-            </div>
-            <div> 
-                  <img  alt="Logo-groupomania" src="../assets/pub/doc-4.jpg" class="projPrev__groupo__masq"> 
-            </div>
-            <div> 
-                  <img  alt="Logo-groupomania" src="../assets/pub/doc-5.jpg" class="projPrev__groupo__masq"> 
-            </div>
-             <div> 
-                  <img  alt="Logo-groupomania" src="../assets/pub/doc-6.jpg" class="projPrev__groupo__masq"> 
-            </div> 
-            <div> 
-                  <img  alt="Logo-groupomania" src="../assets/pub/doc-7.jpg" class="projPrev__groupo__masq"> 
-            </div>
-             <div> 
-                  <img  alt="Logo-groupomania" src="../assets/pub/doc-8.jpg" class="projPrev__groupo__masq"> 
-            </div>
-
-       
+    <div class="app projPrev__groupobis">
+                  <img  alt="Logo-groupomania" src="../assets/pub/doc-1.jpg" class="projPrev__groupo__calque"> 
+                  <img  alt="Logo-groupomania" src="../assets/pub/doc-2.jpg" class="projPrev__groupo__calque"> 
+                  <img  alt="Logo-groupomania" src="../assets/pub/doc-3.jpg" class="projPrev__groupo__calque"> 
+                  <img  alt="Logo-groupomania" src="../assets/pub/doc-4.jpg" class="projPrev__groupo__calque"> 
+                  <img  alt="Logo-groupomania" src="../assets/pub/doc-5.jpg" class="projPrev__groupo__calque"> 
+                  <img  alt="Logo-groupomania" src="../assets/pub/doc-6.jpg" class="projPrev__groupo__calque"> 
+                  <img  alt="Logo-groupomania" src="../assets/pub/doc-7.jpg" class="projPrev__groupo__calque"> 
+                  <img  alt="Logo-groupomania" src="../assets/pub/doc-8.jpg" class="projPrev__groupo__calque"> 
     </div>
 </template>
     
